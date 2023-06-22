10cf1a2703a844e446ab16aec984bfd851ac3e54
LuisRonquillo97 <lronquillo977@gmail.com>
2023-06-21 21:38:37 -0600

    portrait fix, date fix

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ const date = new Date(2023, 7, 19, 18, 30);
 function GetTimeRemaining(){
   var delta = new Date(date - new Date());
   return {
-    months: delta.getMonth() + 1,
+    months: delta.getMonth(),
     days: delta.getDate(),
     hours: delta.getHours(),
     minutes: delta.getMinutes(),
