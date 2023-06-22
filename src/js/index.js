d2f742e228ccd591b04aed97fc3f6c60cab09c2f
LuisRonquillo97 <lronquillo977@gmail.com>
2023-06-21 18:50:39 -0600

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+const date = new Date(2023, 7, 19, 18, 30);
+function GetTimeRemaining(){
+  var delta = new Date(date - new Date());
+  return {
+    months: delta.getMonth() + 1,
+    days: delta.getDate(),
+    hours: delta.getHours(),
+    minutes: delta.getMinutes(),
+    seconds: delta.getSeconds()
+  }
+}
